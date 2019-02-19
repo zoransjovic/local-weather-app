e770c7774342086b4b142e47a8a70f029e822eeb
Zoran Jovic <zoransjovic@gmail.com>
2019-02-19 12:48:04 +0100

    concern to docker

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM zoransjovic/localcast-weather
+WORKDIR /usr/src/app
+COPY dist public
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
   production: true,
+  appId: 'bd90b195e0bd82fbe27cde2ff8ef2c9f',
+  baseUrl: 'http://',
 }
